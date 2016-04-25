50e80c9e6586e96ef023df4b53f7d0b5f4183014
nicolas <nicolas.velasquezgiron@gmail.com>
2016-04-25 13:48:01 -0500

    v 0.2.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ ext2 = Extension(name = 'models',
 
 setup(
     name='wmf',
-    version='0.2.0',
+    version='0.2.1',
     author='Nicolas Velasquez G',
     author_email='nicolas.velasquezgiron@gmail.com',    
     packages=['wmf'],
