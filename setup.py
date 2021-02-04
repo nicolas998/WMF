47196fd50f50e1a4d23934391df8eb42c7936720
Nicolas998 <nicolas.velasquezgiron@gmail.com>
2021-02-04 00:12:34 -0600

    includes new function "dem_process" that obtains the DIR map directly from a DEM

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ ext2 = Extension(name = 'models',
 
 setup(
     name='wmf',
-    version='0.4.8',
+    version='1.0',
     author='Nicolas Velasquez G',
     author_email='nicolas.velasquezgiron@gmail.com',    
     packages=['wmf'],
